e1cf4f960ba79e98c31ffe57043ff65594a28d9e
paula cavender <you@example.com>
2023-01-17 15:54:25 +0000

    style: font testing

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -2,7 +2,7 @@ const Header = () => {
   return (
     <>
       <div className="header-logo">
-        <img src={"./assets/sourcecode-upper.png"} alt="logo"></img>
+        <img src={"./assets/georgia-lower.png"} alt="logo"></img>
         <h1>Paula Jayne Cavender</h1>
       </div>
       <h1>Front end web developer</h1>
