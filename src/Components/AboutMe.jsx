aa0cbb2abc27874b6638423aac87358f1265365a
paula cavender <you@example.com>
2023-01-18 14:27:11 +0000

    further screen layouts

--- src/Components/AboutMe.jsx
+++ src/Components/AboutMe.jsx
@@ -7,7 +7,10 @@ const AboutMe = () => {
         <p>
           Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facilis ut
           adipisci consequatur blanditiis asperiores possimus vitae maiores
-          molestias, debitis quae, tempora labore.
+          molestias, debitis quae, tempora labore. Lorem ipsum dolor sit amet
+          consectetur, adipisicing elit. Facilis ut adipisci consequatur
+          blanditiis asperiores possimus vitae maiores molestias, debitis quae,
+          tempora labore.
         </p>
         <p>check out my linkedin profile</p>
       </div>
